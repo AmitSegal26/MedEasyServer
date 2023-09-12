11ea624449481a7b2ad71b1d4b228c23ab4a2a29
Amit <amitseg2612@gmail.com>
2023-09-12 20:26:59 +0300

    handling the false production.json file

--- model/mongodb/connectToDB.js
+++ model/mongodb/connectToDB.js
@@ -4,6 +4,7 @@ const chalk = require("chalk");
 
 //*aesthetics of console ()
 console.log("");
+
 console.log(
   `connection DB MongoDB => ${
     config.get("dbConfig.url") &&
@@ -14,6 +15,24 @@ console.log(
 );
 
 const connectToDB = () => {
+  //the loop below is just for the project checker, that replacing the procedure of production.json file wont be missed
+  if (config.get("dbConfig.url") === "PLEASE-REPLACE-THE-FILE") {
+    for (let i = 0; i < 10; i++) {
+      console.log(
+        `${chalk
+          .bgHex(i * 123)
+          .hex(i * 763)
+          .bold(
+            "OH! It looks like there is the wrong 'production.json' file!"
+          )}\n${chalk
+          .bgHex(i * 123)
+          .hex(`${(i * 763).toString(16)}`)
+          .bold(
+            "I fear you must replace the production file with the one \nattached to the student's assignment"
+          )} ${chalk.bgBlue.red.bold(":)")}`
+      );
+    }
+  }
   return mongoose.connect(config.get("dbConfig.url"));
 };
 
